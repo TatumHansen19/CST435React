52312cc4c2e984ae220d49089bcd1bf7e03672d9
Tatum Hansen <tatumhansen19@gmail.com>
2025-10-31 09:45:06 -0700

    RNN Fille updates and root fixes

---
+++ RNN/frontend/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+// add plugins like react if this is a React app:
+// import react from "@vitejs/plugin-react";
+export default defineConfig({
+  // plugins: [react()],
+  base: "/rnn/"
+});
